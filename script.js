727fd051d6ef5c805b5326d7877a1b0bad23b57e
HaanimP <haanimpietersen@gmail.com>
2024-06-24 11:32:25 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Function to toggle menu visibility
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Function to open link in new tab
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
